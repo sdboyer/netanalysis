7ed9f351b21061927a4240a17a7250c800bfef36
Sam Boyer (sdboyer) <sam@samboyer.org>
2008-11-30 18:57:33 -0600

    Just familiarizing myself with the class currently; also, fixed stupid mistake with defining the include path.

--- devlistparser.php
+++ devlistparser.php
@@ -1,5 +1,16 @@
 <?php
 
 // Load the general includes file.
-require_once realpath(__FILE__) . '/includes/gen.inc';
+require_once dirname(realpath(__FILE__)) . '/includes/gen.inc';
+load_includes();
 
+$file = '/home/sdboyer/Documents/Academic/Padgett.PLSC57500/final/devarchive/2005-January.txt';
+
+$archive = new MailmanArchiveParser($file);
+$dat = introspect_spl_class($archive);
+
+foreach ($archive as $line) {
+  echo $line;
+}
+
+$i = 'break on me';
